1067156e033d96040f9166c42013771a8988fbab
Keshav Kumar <keshavkumar0610@gmail.com>
2018-04-12 19:43:21 +0530

    Update Teacher_Assignment.c

--- Teacher_Assignment.c
+++ Teacher_Assignment.c
@@ -2,36 +2,42 @@
 #include<stdbool.h>
 struct requirement
 {
-	bool pen= 0;
-	bool paper = 0;
-	bool question_paper=0;
-	bool all_three=0;
+ 	bool pen ;
+	bool paper ;
+	bool question_paper ;
+	bool all_three ;
 };
 int main()
 {
 	int n=3;
 	struct reqirement s[n];
-	s[0].pen=1;				// first student has pen
-	s[1].paper=1;				// second student has paper
-	s[2].question_paper=1;				// third student has question paper
-	while(s[0].all_three==0||s[1].all_three==0||s[2].all_three==0)
+	s[0].pen=true;		
+	s[0].paper = false;
+	s[0].question_paper = false;
+	s[1].pen=false;		
+	s[1].paper = true;
+	s[1].question_paper = false;
+	s[2].pen=false;		
+	s[2].paper = false;
+	s[2].question_paper = true;	
+	while(s[0].all_three==false||s[1].all_three==false||s[2].all_three==false)
 	{
 		char ch1,ch2;
 		printf("Menu: \n1.pen\n2.paper\n3.question paper\n Enter the two things which is to be placed on the shared table");
 		scanf("%c%c",&ch1,&ch2);
-		if(ch1=='1' && ch2=='2' && s[2].all_three==0)
+		if(ch1=='1' && ch2=='2' && s[2].all_three==false)
 		{
-			s[2].all_three=1;
+			s[2].all_three=true ;
 			printf("Student three has completed the task");
 		}
-		if(ch1=='2' && ch2=='3's[0].all_three==0)
+		if(ch1=='2' && ch2=='3' && s[0].all_three==false)
 		{
-			s[0].all_three=1;
+			s[0].all_three=true;
 			printf("Student one has completed the task");
 		}
-		if(ch1=='1' && ch2=='3's[1].all_three==0)
+		if(ch1=='1' && ch2=='3' && s[1].all_three==false)
 		{
-			s[1].all_three=1;
+			s[1].all_three=true;
 			printf("Student second has completed the task");
 		}
 	}
