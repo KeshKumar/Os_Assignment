f19ab9b368fb0516266f8a9a878f4c61f3f5cb5a
Keshav Kumar <keshavkumar0610@gmail.com>
2018-04-13 01:27:14 +0530

    Update Teacher_Assignment.c

--- Teacher_Assignment.c
+++ Teacher_Assignment.c
@@ -2,10 +2,10 @@
 #include<stdbool.h>
 struct requirement
 {
- 	boolean pen ;
-	boolean paper ;
-	boolean question_paper ;
-	boolean all_three ;
+ 	bool pen ;
+	bool paper ;
+	bool question_paper ;
+	bool all_three ;
 };
 int main()
 {
