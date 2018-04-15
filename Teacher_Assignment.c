5efe75e96b27e50047e8235742f43dc5b7078996
Keshav Kumar <keshavkumar0610@gmail.com>
2018-04-15 12:26:00 +0530

    Update Teacher_Assignment.c

--- Teacher_Assignment.c
+++ Teacher_Assignment.c
@@ -15,37 +15,40 @@ struct requirement
 int main()
 {
 	int n=3;
-	struct reqirement s[n];
+	struct requirement  s[n];
 	s[0].pen=true;		
 	s[0].paper = false;
 	s[0].question_paper = false;
+	s[0].all_three= false;
 	s[1].pen=false;		
 	s[1].paper = true;
 	s[1].question_paper = false;
+	s[1].all_three = false;
 	s[2].pen=false;		
 	s[2].paper = false;
-	s[2].question_paper = true;	
+	s[2].question_paper = true;
+	s[2].all_three = false	;
 	while(s[0].all_three==false||s[1].all_three==false||s[2].all_three==false)
 	{
-		char ch1,ch2;
-		printf("Menu: \n1.pen\n2.paper\n3.question paper\n Enter the two things which is to be placed on the shared table");
-		scanf("%c%c",&ch1,&ch2);
-		if(ch1=='1' && ch2=='2' && s[2].all_three==false)
+		int ch1,ch2;
+		printf("Menu:\n1.pen\n2.paper\n3.question paper\n Enter the two things which is to be placed on the shared table ");
+		scanf("%d%d",&ch1,&ch2);
+		if(ch1==1 && ch2==2 && s[2].all_three==false)
 		{
 			s[2].all_three=true ;
-			printf("Student three has completed the task");
+			printf("Student three has completed the task\n");
 		}
-		if(ch1=='2' && ch2=='3' && s[0].all_three==false)
+		if(ch1==2 && ch2==3 && s[0].all_three==false)
 		{
 			s[0].all_three=true;
-			printf("Student one has completed the task");
+			printf("Student one has completed the task\n");
 		}
-		if(ch1=='1' && ch2=='3' && s[1].all_three==false)
+		if(ch1==1 && ch2==3 && s[1].all_three==false)
 		{
 			s[1].all_three=true;
-			printf("Student second has completed the task");
+			printf("Student second has completed the task\n");
 		}
 	}
-	printf("All students completed their respective tasks");
+	printf("All students completed their respective tasks\n");
 	return 0;
 }
